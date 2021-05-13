cf4a71217347aef4ab2783d229ce6dd23e65ab03
Przemo <przemo06133@gmail.com>
2021-05-13 19:10:49 +0200

    add load font

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,13 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, StyleSheet } from 'react-native';
+import * as Font from 'expo-font';
+import AppLoading from 'expo-app-loading';
+
+const FetchFonts = () => {
+	Font.loadAsync({
+		'open-sans': require('../assets/fonts/OpenSans-Regular.tff'),
+		'open-sans-bold': require('../assets/fonts/OpenSans-Bold.tff'),
+	});
+};
 
 const CategoryMealsScreen = () => {
+	const [fontLoaded, setFontLaded] = useState(false);
+
+	if (!fontLoaded) {
+		return (
+			<AppLoading
+				startAsync={fetchFonts}
+				onFinish={() => setFontLoaded(true)}
+				onError={(err) => console.log(err)}
+			/>
+		);
+	}
+
 	return (
 		<View style={styles.screen}>
 			<Text>CategoryMealsScreen</Text>
 		</View>
 	);
 };
+
+<AppLoading
+	startAsync={fetchFonts}
+	onFinish={() => setFontLoaded(true)}
+	onError={(err) => console.log(err)}
+/>;
 
 const styles = StyleSheet.create({
 	screen: {
