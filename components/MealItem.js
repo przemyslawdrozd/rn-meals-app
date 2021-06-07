ba7ed819866a935e46fc90994bf3ce48b844c096
Przemo <przemo06133@gmail.com>
2021-06-07 16:17:59 +0200

    style meal details screen

--- components/MealItem.js
+++ components/MealItem.js
@@ -1,76 +1,77 @@
-import React from 'react';
+import React from "react";
 import {
-	View,
-	Text,
-	TouchableOpacity,
-	StyleSheet,
-	ImageBackground,
-} from 'react-native';
+  View,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  ImageBackground,
+} from "react-native";
+import DefaultText from "./DefaultText";
 
 const MealItem = ({ item, onSelectMeal }) => {
-	return (
-		<View style={styles.mealItem}>
-			<TouchableOpacity onPress={onSelectMeal}>
-				<View>
-					<View style={{ ...styles.mealRow, ...styles.mealHeader }}>
-						<ImageBackground
-							style={styles.bgImage}
-							source={{ uri: item.imageUrl }}
-						>
-							<View style={styles.titleContainer}>
-								<Text style={styles.title} numberOfLines={1}>
-									{item.title}
-								</Text>
-							</View>
-						</ImageBackground>
-					</View>
-					<View style={{ ...styles.mealRow, ...styles.mealDetail }}>
-						<Text>{item.duration}m</Text>
-						<Text>{item.complexity.toUpperCase()}</Text>
-						<Text>{item.affodability.toUpperCase()}</Text>
-					</View>
-				</View>
-			</TouchableOpacity>
-		</View>
-	);
+  return (
+    <View style={styles.mealItem}>
+      <TouchableOpacity onPress={onSelectMeal}>
+        <View>
+          <View style={{ ...styles.mealRow, ...styles.mealHeader }}>
+            <ImageBackground
+              style={styles.bgImage}
+              source={{ uri: item.imageUrl }}
+            >
+              <View style={styles.titleContainer}>
+                <Text style={styles.title} numberOfLines={1}>
+                  {item.title}
+                </Text>
+              </View>
+            </ImageBackground>
+          </View>
+          <View style={{ ...styles.mealRow, ...styles.mealDetail }}>
+            <DefaultText>{item.duration}m</DefaultText>
+            <DefaultText>{item.complexity.toUpperCase()}</DefaultText>
+            <DefaultText>{item.affodability.toUpperCase()}</DefaultText>
+          </View>
+        </View>
+      </TouchableOpacity>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-	mealItem: {
-		height: 200,
-		width: '100%',
-		backgroundColor: '#f5f5f5',
-		borderRadius: 10,
-		overflow: 'hidden',
-		marginVertical: 10,
-	},
-	mealRow: {
-		flexDirection: 'row',
-	},
-	mealHeader: {
-		height: '85%',
-	},
-	mealDetail: {
-		paddingHorizontal: 10,
-		justifyContent: 'space-between',
-		alignItems: 'center',
-		height: '15%',
-	},
-	bgImage: {
-		width: '100%',
-		height: '100%',
-		justifyContent: 'flex-end',
-	},
-	titleContainer: {
-		backgroundColor: 'rgba(0,0,0,0.5)',
-		paddingVertical: 5,
-	},
-	title: {
-		color: 'white',
-		fontFamily: 'open-sans-bold',
-		fontSize: 22,
-		textAlign: 'center',
-	},
+  mealItem: {
+    height: 200,
+    width: "100%",
+    backgroundColor: "#f5f5f5",
+    borderRadius: 10,
+    overflow: "hidden",
+    marginVertical: 10,
+  },
+  mealRow: {
+    flexDirection: "row",
+  },
+  mealHeader: {
+    height: "85%",
+  },
+  mealDetail: {
+    paddingHorizontal: 10,
+    justifyContent: "space-between",
+    alignItems: "center",
+    height: "15%",
+  },
+  bgImage: {
+    width: "100%",
+    height: "100%",
+    justifyContent: "flex-end",
+  },
+  titleContainer: {
+    backgroundColor: "rgba(0,0,0,0.5)",
+    paddingVertical: 5,
+  },
+  title: {
+    color: "white",
+    fontFamily: "open-sans-bold",
+    fontSize: 22,
+    textAlign: "center",
+  },
 });
 
 export default MealItem;
--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,52 +1,90 @@
-import React from 'react';
-import { View, Text, Button, StyleSheet } from 'react-native';
-import { HeaderButtons, Item } from 'react-navigation-header-buttons';
-import HeaderButton from '../components/HeaderButton';
-import { MEALS } from '../data/data';
+import React from "react";
+import {
+  ScrollView,
+  View,
+  Image,
+  Text,
+  Button,
+  StyleSheet,
+} from "react-native";
+import { HeaderButtons, Item } from "react-navigation-header-buttons";
+import HeaderButton from "../components/HeaderButton";
+import { MEALS } from "../data/data";
+import DefaultText from "../components/DefaultText";
+
+const ListItem = ({ children }) => (
+  <View style={styles.listItem}>
+    <DefaultText>{children}</DefaultText>
+  </View>
+);
 
 const MealDetailScreen = ({ navigation }) => {
-	const mealId = navigation.getParam('mealId');
-	console.log('mealId ', mealId);
+  const mealId = navigation.getParam("mealId");
+  console.log("mealId ", mealId);
 
-	const selectedMeal = MEALS.find((meal) => mealId === meal.id);
+  const selectedMeal = MEALS.find((meal) => mealId === meal.id);
 
-	return (
-		<View style={styles.screen}>
-			<Text>{selectedMeal.title}</Text>
-			<Button
-				title='Go back to categories!'
-				onPress={() => {
-					navigation.popToTop();
-				}}
-			/>
-		</View>
-	);
+  return (
+    <ScrollView>
+      <Image style={styles.image} source={{ uri: selectedMeal.imageUrl }} />
+      <View style={styles.details}>
+        <DefaultText>{selectedMeal.duration}m</DefaultText>
+        <DefaultText>{selectedMeal.complexity.toUpperCase()}</DefaultText>
+        <DefaultText>{selectedMeal.affodability.toUpperCase()}</DefaultText>
+      </View>
+      <Text style={styles.title}>Ings:</Text>
+      {selectedMeal.ings.map((ing) => (
+        <ListItem key={ing}>{ing}</ListItem>
+      ))}
+      <Text style={styles.title}>Steps:</Text>
+      {selectedMeal.steps.map((step) => (
+        <ListItem key={step}>{step}</ListItem>
+      ))}
+    </ScrollView>
+  );
 };
 
 MealDetailScreen.navigationOptions = (navigationData) => {
-	const mealId = navigationData.navigation.getParam('mealId');
+  const mealId = navigationData.navigation.getParam("mealId");
 
-	const selectedMeal = MEALS.find((meal) => meal.id === mealId);
-	return {
-		headerTitle: selectedMeal.title,
-		headerRight: (
-			<HeaderButtons HeaderButtonComponent={HeaderButton}>
-				<Item
-					title='Favorite'
-					iconName='ios-star'
-					onPress={() => console.log('MArk as favorite')}
-				/>
-			</HeaderButtons>
-		),
-	};
+  const selectedMeal = MEALS.find((meal) => meal.id === mealId);
+  return {
+    headerTitle: selectedMeal.title,
+    headerRight: (
+      <HeaderButtons HeaderButtonComponent={HeaderButton}>
+        <Item
+          title="Favorite"
+          iconName="ios-star"
+          onPress={() => console.log("MArk as favorite")}
+        />
+      </HeaderButtons>
+    ),
+  };
 };
 
 const styles = StyleSheet.create({
-	screen: {
-		flex: 1,
-		justifyContent: 'center',
-		alignItems: 'center',
-	},
+  image: {
+    width: "100%",
+    height: 200,
+  },
+
+  details: {
+    flexDirection: "row",
+    padding: 15,
+    justifyContent: "space-around",
+  },
+  title: {
+    fontFamily: "open-sans-bold",
+    textAlign: "center",
+    fontSize: 22,
+  },
+  listItem: {
+    marginVertical: 10,
+    marginHorizontal: 20,
+    borderColor: "#ccc",
+    borderWidth: 1,
+    padding: 10,
+  },
 });
 
 export default MealDetailScreen;
